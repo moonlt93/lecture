17eb9bec0794b5274ba9a873a01e5b3af11b30f4
진수 <moonlt93@naver.com>
2024-12-25 18:49:44 +0900

    feat: 비관적 lock 적용

--- src/main/kotlin/com/my/sparta/lecture/repository/orm/jpa/UserSchedulerJpaRepository.kt
+++ src/main/kotlin/com/my/sparta/lecture/repository/orm/jpa/UserSchedulerJpaRepository.kt
@@ -1,17 +1,21 @@
 package com.my.sparta.lecture.repository.orm.jpa
 
 import com.my.sparta.lecture.domain.entity.UserScheduler
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
-import java.util.*
 
 interface UserSchedulerJpaRepository : JpaRepository<UserScheduler, Long> {
 
-    @Query("""
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query(
+        """
         SELECT us 
         FROM UserScheduler us 
         WHERE us.scheduler.id = :schedulerId AND us.users.id = :userId
-    """)
+    """
+    )
     fun findByLectureIdAndUserId(schedulerId: String, userId: String): UserScheduler?
 
     @Query("select uc from UserScheduler as uc where uc.users.id = :userId")
